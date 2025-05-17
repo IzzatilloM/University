c52edc207ffd2d831e410be209c041b7e15157ab
Izzatillo <musayevizzatillo1@gmail.com>
2025-05-17 14:34:42 +0500

    viewlar qoshildi

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,16 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
+from univer.views import home_view, yunalish_view, ustozlar_view, fanlar_view, yunalish_confirm_delete_view, yunalish_delete_view, fanlar_delete_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home_view),
+    path('yunalish/',yunalish_view),
+    path('yunalish/<int:pk>/confirm-delete/', yunalish_confirm_delete_view),
+    path('yunalish/<int:pk>/delete/', yunalish_delete_view),
+    path('ustozlar/',ustozlar_view),
+    path('fanlar/',fanlar_view),
+    path('fanlar/<int:pk>/delete/', fanlar_delete_view),
+
 ]
--- univer/views.py
+++ univer/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
 
-# Create your views here.
+from univer.models import *
+
+
+def home_view(request):
+    return render(request,'index.html')
+
+def yunalish_view(request):
+    yunalish = Yunalish.objects.all()
+    if request.method == 'POST':
+        Yunalish.objects.create(
+            nom=request.get.POST('nom'),
+            aktiv=True if request.POST.get('aktiv') else request.POST.get('aktiv'))
+        return redirect('/yunalish/')
+    context = {
+        'yunalish':yunalish,
+    }
+    return render(request,'yunalish.html',context)
+
+def ustozlar_view(request):
+    ustozlar = Ustoz.objects.all()
+    context = {
+        'ustozlar':ustozlar,
+    }
+    return render(request,'ustozlar.html',context)
+
+def fanlar_view(request):
+    fanlar = Fan.objects.all()
+    yunalishlar = Yunalish.objects.order_by('nom')
+    if request.method == 'POST':
+        Fan.objects.create(
+        nom=request.POST.get('nom'),
+        asosiy=True if request.POST.get('asosiy') else request.Post.get('asosiy'),
+        yunalish=Yunalish.objects.get(id=request.POST.get('yunalish_id')))
+        return redirect('/fanlar/')
+    context = {
+        'fanlar':fanlar,
+        'yunalishlar':yunalishlar,
+    }
+    return render(request,'fanlar.html',context)
+
+def yunalish_confirm_delete_view(request,pk):
+    yunalish  = get_object_or_404(Yunalish,pk=pk)
+    context = {
+        'yunalish':yunalish,
+    }
+    return render(request,'yunalish_confirm_delete.html',context)
+
+def yunalish_delete_view(request,pk):
+    yunalish = get_object_or_404(Yunalish,pk=pk)
+    yunalish.delete()
+    return redirect('/yunalish/')
+
+def fanlar_delete_view(request,pk):
+    fanlar = get_object_or_404(Fan,pk=pk)
+    fanlar.delete()
+    return redirect('/fanlar/')
